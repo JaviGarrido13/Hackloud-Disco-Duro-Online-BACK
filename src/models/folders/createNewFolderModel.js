33c652fb9ceb246b04cde22aa6a090f5e30ec273
Bridinia Aguero <bridiniaaguero@gmail.com>
2025-02-14 02:14:07 +0100

    Cambios creacion folders

--- src/models/folders/createNewFolderModel.js
+++ src/models/folders/createNewFolderModel.js
@@ -1,9 +1,4 @@
-import path from "path";
 import { getPool } from "../../db/getpool.js"
-import fs from 'fs'
-
-
-const folderSqlPath = path.resolve("uploads");
 
 export const createNewFolderModel = async (foldersId, foldersName, foldersUserId) => {
 
@@ -16,24 +11,6 @@ export const createNewFolderModel = async (foldersId, foldersName, foldersUserId
         `,
         [foldersId, foldersName, foldersUserId]
     );
-
-    try {
-        // Crear la ruta completa de la carpeta
-        const folderPath = path.join(folderSqlPath, foldersId);
-
-        // Verificar si la carpeta existe antes de crearla
-        try {
-            await fs.access(folderPath);
-            console.log(`La carpeta '${foldersName}' con ID ${foldersId} ya existe.`);
-        } catch {
-            await fs.mkdir(folderPath);
-            console.log(`Carpeta '${foldersName}' creada en: ${folderPath}`);
-        }
-
-    } catch (error) {
-        console.error(`Error al crear la carpeta: ${error}`);
-        throw error;
-    }
 
     return result;
 };
