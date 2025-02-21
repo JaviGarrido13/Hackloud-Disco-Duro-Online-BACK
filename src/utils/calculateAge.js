d84f375c59382e7cd4bd02df45ff6683290e651e
javi.garrido <fjgarridovalderas@hotmail.com>
2025-02-21 17:19:30 +0100

    cambio calculo Edad

--- src/utils/calculateAge.js
+++ src/utils/calculateAge.js
@@ -4,7 +4,7 @@ export const calculateAge = (birthday) => {
     // obtenemos la fecha actual
     const hoy = new Date();
     // calculamos la diferencia de años
-    const edad = hoy.getFullYear() - nacimiento.getFullYear();
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
     // si la fecha actual es menor a la fecha de nacimiento, restamos 1 año
     // obtenemos la diferencia de meses
     const mesDiff = hoy.getMonth() - nacimiento.getMonth();
