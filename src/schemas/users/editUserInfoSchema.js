0d6baed04425a4124da008e8ae35097b734178a2
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-02-21 19:00:13 +0100

    Una "s" que faltaba

--- src/schemas/users/editUserInfoSchema.js
+++ src/schemas/users/editUserInfoSchema.js
@@ -3,10 +3,15 @@ import joi from 'joi';
 import joiErrorMessages from '../joiErrorMessages.js';
 
 const editUserInfoSchema = joi.object({
-    username: joi.string().min(3).max(25).required().message(joiErrorMessages),
-    email: joi.string().email().required().message(joiErrorMessages),
-    firstName: joi.string().min(3).max(40).optional().message(joiErrorMessages),
-    lastName: joi.string().min(3).max(40).optional().message(joiErrorMessages),
+    username: joi.string().min(3).max(25).required().messages(joiErrorMessages),
+    email: joi.string().email().required().messages(joiErrorMessages),
+    firstName: joi
+        .string()
+        .min(3)
+        .max(40)
+        .optional()
+        .messages(joiErrorMessages),
+    lastName: joi.string().min(3).max(40).optional().messages(joiErrorMessages),
 });
 
 export default editUserInfoSchema;
