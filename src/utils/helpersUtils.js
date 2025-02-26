5a3720ba8c330a05f999d2fafb5fe1627c31f1cf
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-02-26 19:43:13 +0100

    retoque

--- src/utils/helpersUtils.js
+++ src/utils/helpersUtils.js
@@ -1,11 +1,24 @@
-const generateErrorUtils = (status, code, message) => {
-    const error = new Error();
-    error.httpStatus = typeof status === 'number' ? status : 500;
-    error.code = typeof code === 'string' ? code : 'INTERNAL_SERVER_ERROR';
-    error.message =
-        typeof message === 'string' ? message : 'Ocurrió un error inesperado.';
+// const generateErrorUtils = (status, code, message) => {
+// const error = new Error();
+// error.httpStatus = typeof status === 'number' ? status : 500;
+// error.code = typeof code === 'string' ? code : 'INTERNAL_SERVER_ERROR';
+// error.message =
+// typeof message === 'string' ? message : 'Ocurrió un error inesperado.';
+//
+// return error;
+// };
+//
+// export default generateErrorUtils;
 
-    return error;
+const generateErrorUtils = (status, code, message) => {
+    throw {
+        httpStatus: typeof status === 'number' ? status : 500,
+        code: typeof code === 'string' ? code : 'INTERNAL_SERVER_ERROR',
+        message:
+            typeof message === 'string'
+                ? message
+                : 'Ocurrió un error inesperado.',
+    };
 };
 
 export default generateErrorUtils;
